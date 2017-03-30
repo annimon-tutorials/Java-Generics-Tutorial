0c78b8e140b39d3905eb7e49444f0d806f7d6292
Victor <annimon119@gmail.com>
2017-03-30 15:30:31 +0300

    Logger list with unknown type wildcard

--- src/main/java/com/example/generics/step9/Logger.java
+++ src/main/java/com/example/generics/step9/Logger.java
@@ -7,13 +7,13 @@ public class Logger {
 
     public static void main(String[] args) {
         List<Number> numbers = Arrays.asList(0, 0.5, 12.3, 30, 450);
+        Logger.print(numbers);
 
-        // Error: incompatible types: java.util.List<java.lang.Number>
-        //        cannot be converted to java.util.List<java.lang.Object>
-        // Logger.print(numbers);
+        List<Object> objects = Arrays.asList("string", new Object[5], null, 15);
+        Logger.print(objects);
     }
 
-    public static void print(List<Object> list) {
+    public static void print(List<?> list) {
         for (Object object : list) {
             log(object);
         }
